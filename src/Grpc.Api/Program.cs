cfc261a679c1a18ede928e8f65f56ac634013dfe
Aleksei Mialkin <aleksei@mialkin.ru>
2024-10-15 13:16:12 +0300

    Use AddGrpcClient

--- src/Grpc.Api/Program.cs
+++ src/Grpc.Api/Program.cs
@@ -1,28 +1,19 @@
-using Grpc.Net.Client;
 using Grpc.Server;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+var services = builder.Services;
+services.AddGrpcClient<Greeter.GreeterClient>(x => { x.Address = new Uri("http://localhost:5140"); });
 
 var application = builder.Build();
-application.UseSwagger();
-application.UseSwaggerUI();
 
-application.MapGet("/", () => "ping")
-    .WithName("get-ping")
-    .WithOpenApi();
+application.MapGet("/", () => "ping");
 
 application.MapGet(
-    "/get-name", async () =>
+    "/get-name", async (Greeter.GreeterClient client) =>
     {
-        using var channel = GrpcChannel.ForAddress(address: "http://localhost:5140");
-
-        var client = new Greeter.GreeterClient(channel);
         var request = new GetGreetingRequest();
         var result = await client.GetGreetingAsync(request);
-
         return result;
     });
 
