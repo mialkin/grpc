5e6e6c91cf0cdebfee0bc8df04cc3c1302fc589c
Aleksei Mialkin <aleksei@mialkin.ru>
2024-10-16 23:17:40 +0300

    Use Scalar.AspNetCore

--- src/Grpc.Api/Program.cs
+++ src/Grpc.Api/Program.cs
@@ -1,13 +1,26 @@
 using Grpc.Server;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var services = builder.Services;
 services.AddGrpcClient<Greeter.GreeterClient>(x => { x.Address = new Uri("http://localhost:5140"); });
 
 var application = builder.Build();
 
-application.MapGet("/", () => "ping");
+application.UseSwagger(options => { options.RouteTemplate = "openapi/{documentName}.json"; });
+
+application.MapScalarApiReference(
+    x =>
+    {
+        x.Title = "GRPC client API";
+        x.DarkMode = false;
+    });
+
+application.MapGet("/", () => Results.Redirect("/scalar/v1")).ExcludeFromDescription();
 
 application.MapGet(
     pattern: "/call-server",
